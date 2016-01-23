ad118c575e1e2646ed1c75d5c16d8339c7db2f24
Kyle Eslick <mii756@gmail.com>
2016-01-23 16:42:17 -0700

    added form.php

--- form.php
+++ form.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+if ($_SESSION["submit"]) {
+    header('Location: responce.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +17,7 @@
 
 <body>
 
-    <div class="container">
+    <div style="width: 100%; " class="container">
         <!-- Static navbar -->
         <nav class="navbar navbar-default">
             <div class="container-fluid">
@@ -55,8 +61,69 @@
 
     <div class="container">
         <div class="container tinted round">
-            <h1>Homework</h1>
-            <a href="form.php">PHP SURVEY</a>
+            <h1>Computer Information</h1>
+
+            <form action="responce.php" method="post" id="myForm" name="myForm">
+
+                <label>Processor brand:</label>
+                <div class="radio">
+                    <label>    
+                        <input type="radio" name="processor" value="AMD"> AMD
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="processor" value="Intel"> Intel
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    <label for="speedInput">Processor Speed (GHz)</label>
+                    <input type="text" id="speedInput" name="processorSpeed" class="form-control">
+                </div>
+
+                <label for="ramInput">Ram:</label>
+                <select name="ram" id="ramInput" class="form-control">
+                    <option value="2">2 GB</option>
+                    <option value="4">4 GB</option>
+                    <option value="6">6 GB</option>
+                    <option value="8" selected>8 GB</option>
+                    <option value="12">12 GB</option>
+                    <option value="16">16 GB</option>
+                    <option value="32">32 GB</option>
+                    <option value="64">64 GB</option>
+                </select>
+
+                <label>Video Card brand:</label>
+                <div class="radio">
+                    <label>    
+                        <input type="radio" name="videoCard" value="AMD"> AMD
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>    
+                        <input type="radio" name="videoCard" value="Nvidia"> Nvidia
+                    </label>
+                </div>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="videoCard" value="Intel"> Intel
+                    </label>
+                </div>
+
+                <label for="usesInput">What do you use your computer for?</label>
+                <div class="form-group">
+                    <input type="checkbox" name="uses[]" value="Games"> Games<br/>
+                    <input type="checkbox" name="uses[]" value="Video Processing"> Video Processing<br/>
+                    <input type="checkbox" name="uses[]" value="Web/Cat Videos"> Web/Cat Videos<br/>
+                    <input type="checkbox" name="uses[]" value="Other"> Other<br/>
+                </div>
+
+                <div class="form-group" id="submit">
+                    <button type="submit" class="btn btn-default" >Submit</button>
+                    <a href="responce.php" class="btn btn-default" >View Results</a>
+                </div>
+            </form>
         </div>
 
         <footer class="footer">
---
+++ responce.php
@@ -0,0 +1,274 @@
+<?php
+session_start();
+
+$processor = $processorSpeed = $ram = $videoCard = $uses = "";
+if( isset($_POST["processor"]) &&
+    isset($_POST["processorSpeed"]) &&
+    isset($_POST["ram"]) &&
+    isset($_POST["videoCard"]) &&
+    isset($_POST["uses"])) {
+        $_SESSION["submit"] = true;
+        $processor = $_POST["processor"];
+        $processorSpeed = $_POST["processorSpeed"];
+        $ram = $_POST["ram"];
+        $videoCard = $_POST["videoCard"];
+        $uses = $_POST["uses"];
+        $arr = array('processor' => $processor,
+                    'processorSpeed' => $processorSpeed,
+                    'ram' => $ram,
+                    'videoCard' => $videoCard,
+                    'uses' => $uses);
+        write($arr);
+}
+
+function read(){
+    return file_get_contents("results.txt");
+}
+function write($arr){
+
+    $read = json_decode(read(), true); 
+
+    $arrkey = array_keys($arr);
+    foreach ($arrkey as $temp) {
+        if (is_array($arr[$temp])) {
+            print_r($arr[$temp]);
+            foreach ($arr[$temp] as $value) {
+                $read[$temp][$value] += 1;
+            }
+        } else {
+            if (array_key_exists($arr[$temp], $read[$temp])){
+                $read[$temp][$arr[$temp]] += 1;
+            }
+        }
+    }
+
+    if (!array_key_exists($arr['processorSpeed'], $read['processorSpeed'])){
+        $read['processorSpeed'] += array($arr['processorSpeed'] => 1);
+    }
+    file_put_contents("results.txt", json_encode($read));
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Kyle Eslick</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/color.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <script type="text/javascript" src="css/canvasjs-1.8.0/canvasjs.min.js"></script>
+    <script type="text/javascript">
+        window.onload = function () {
+            var text = "<?php echo addslashes(read()); ?>";
+            var info = JSON.parse(text);
+            var chart = new CanvasJS.Chart("processor",
+            {
+              title:{
+                text: "Processor Brand"    
+            },
+            data: [
+
+            {        
+                type: "column",  
+                showInLegend: true, 
+                legendMarkerType: "none",
+                legendText: "# of processors by brand",
+                dataPoints: [      
+                { x: 1, y: info["processor"]["AMD"], label: "AMD"},
+                { x: 2, y: info["processor"]["Intel"],  label: "Intel" }
+                ]
+            }
+            ]
+        });
+
+
+            chart.render();
+
+            var chart = new CanvasJS.Chart("ram",
+            {
+                title:{
+                    text: "Ram"
+                },
+                legend: {
+                    maxWidth: 350,
+                    itemWidth: 120
+                },
+                data: [
+                {
+                    type: "pie",
+                    showInLegend: true,
+                    legendText: "{indexLabel}",
+                    dataPoints: [
+                    { y: info["ram"][2], indexLabel: "2 GB" },
+                    { y: info["ram"][4], indexLabel: "4 GB" },
+                    { y: info["ram"][6], indexLabel: "6 GB" },
+                    { y: info["ram"][8], indexLabel: "8 GB" },
+                    { y: info["ram"][12], indexLabel: "12 GB" },
+                    { y: info["ram"][16], indexLabel: "16 GB" },
+                    { y: info["ram"][32], indexLabel: "32 GB" },
+                    { y: info["ram"][64], indexLabel: "64 GB" }
+
+                    ]
+                }
+                ]
+            });
+            chart.render();
+
+            var chart = new CanvasJS.Chart("videoCard",
+            {
+                title:{
+                    text: "Video Card Brand"
+                },
+                legend: {
+                    maxWidth: 350,
+                    itemWidth: 120
+                },
+                data: [
+                {
+                    type: "pie",
+                    showInLegend: true,
+                    legendText: "{indexLabel}",
+                    dataPoints: [
+                    { y: info["videoCard"]["AMD"], indexLabel: "AMD" },
+                    { y: info["videoCard"]["Nvidia"], indexLabel: "Nvidia" },
+                    { y: info["videoCard"]["Intel"], indexLabel: "Intel" }
+
+                    ]
+                }
+                ]
+            });
+            chart.render();
+
+            var chart = new CanvasJS.Chart("uses",
+            {
+              title:{
+                text: "Uses"    
+            },
+            data: [
+
+            {        
+                type: "column",  
+                showInLegend: true, 
+                legendMarkerType: "none",
+                legendText: "# of uses",
+                dataPoints: [      
+                { x: 1, y: info["uses"]["Games"], label: "Games"},
+                { x: 2, y: info["uses"]["Video Processing"],  label: "Video Processing" },
+                { x: 3, y: info["uses"]["Web/Cat Videos"], label: "Web/Cat Videos"},
+                { x: 4, y: info["uses"]["Other"], label: "Other"}
+                ]
+            }
+            ]
+        });
+
+            chart.render();
+        }
+    </script>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- Static navbar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project: Store</a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.html">Home</a></li>
+                        <li class="active"><a href="homework.html">Homework</a></li>
+                        <!--<li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>-->
+                    </ul> 
+                    <!--
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">Default <span class="sr-only">(current)</span></a></li>
+                        <li><a href="../navbar-static-top/">Static top</a></li>
+                        <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+                    </ul> -->
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
+        </nav>
+    </div> <!-- /container -->
+
+    <div class="container">
+        <div class="container tinted round">
+            <?php
+                if (isset($_POST["processor"]) &&
+                    isset($_POST["processorSpeed"]) &&
+                    isset($_POST["ram"]) &&
+                    isset($_POST["videoCard"]) &&
+                    isset($_POST["uses"])) {
+                    echo "<p class=\"text-center\">Thank you for sending your computer's information</p>";
+                }
+                if ($_SESSION["submit"]) {
+                    echo "<p class=\"text-center\">You have already submitted a form.</p>";
+                }
+            ?>
+            <h1>Results</h1>
+            <div id="processor" style="height: 300px; width: 100%;"></div>
+            <div id="processorSpeed">
+                <?php 
+                    $temp = json_decode(read(), true);
+                    $temp = $temp["processorSpeed"];
+                    arsort($temp);
+                    $keys = array_keys($temp);
+                    $count = 0;
+                    echo "<h3>Top Processor Speeds</h3>";
+                    echo "<table class=\"table table-striped\"><tr><th>Number</th><th>Speed</th><th>Count</th></tr>";
+
+                    foreach ($keys as $key) {
+                        $count++;
+                        echo "<tr><td>#" . $count . "</td><td>" . $key . " GHz</td><td>" . $temp[$key] . "</td></tr>";
+                        if ($count > 10) {
+                            break;
+                        }
+                    }
+                    echo "</table>";
+    
+                ?>
+            </div>
+            <div id="ram" style="height: 300px; width: 100%;"></div>
+            <div id="videoCard" style="height: 300px; width: 100%;"></div>
+            <div id="uses" style="height: 300px; width: 100%;"></div>
+            <?php
+
+
+            ?>
+        </div>
+
+        <footer class="footer">
+            <p>&copy; Kyle Eslick</p>
+        </footer>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
